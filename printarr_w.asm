42bee6e6b7c7c1b966d812b933dd114a907ce8f7
Utkarsh Ray <UtkarshRay@gmail.com>
2014-10-27 14:43:48 +0530

    Create printarr_w.asm

---
+++ printarr_w.asm
@@ -0,0 +1,25 @@
+; Prints the array pointed to
+; by si and of size is parameter
+; Uses decprint
+printarr_w proc
+    push bp
+    mov bp, sp
+    push ax
+    push cx
+    push si
+    
+    mov cx, [bp + 4]
+    cmp cx, 0
+    jle cleanup_printarr_w
+    cld
+    printarr_w_loop:
+        lodsw
+        call decprint
+        loop printarr_w_loop
+    cleanup_printarr_w:
+    pop si
+    pop cx
+    pop bx
+    pop ax
+    ret 2   ; return and remove the parameter
+printarr_w endp
